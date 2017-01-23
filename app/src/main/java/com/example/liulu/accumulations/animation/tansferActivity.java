b61a053d77e54cd6a1eb2f80378f76aeffa71ca7
liulu <532922397@qq.com>
2017-01-23 17:30:46 +0800

    Merge remote-tracking branch 'Accumulations/master'
    Conflicts:
    	.idea/gradle.xml
    	.idea/misc.xml
    	.idea/modules.xml
    	app/build.gradle
    	app/src/main/AndroidManifest.xml
    	app/src/main/java/com/example/liulu/accumulations/MainActivity.java
    	app/src/main/res/layout/activity_main.xml
    	app/src/main/res/values/colors.xml
    	app/src/main/res/values/dimens.xml
    	app/src/main/res/values/strings.xml
    	app/src/main/res/values/styles.xml
    	settings.gradle

---
+++ app/src/main/java/com/example/liulu/accumulations/animation/tansferActivity.java
@@ -0,0 +1,18 @@
+package com.example.liulu.accumulations.animation;
+
+import com.example.liulu.accumulations.R;
+import com.example.liulu.accumulations.common.BaseActivity;
+
+public class tansferActivity extends BaseActivity {
+
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public int getLayout() {
+        return R.layout.activity_tansfer;
+    }
+}
---
+++ testlib/src/main/java/com/example/testlib/MyIntentService.java
@@ -0,0 +1,17 @@
+package com.example.testlib;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyIntentService extends IntentService {
+
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.e("TAG", "1111111111111111111111111");
+    }
+}
