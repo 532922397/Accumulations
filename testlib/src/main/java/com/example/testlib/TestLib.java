e657853ae27c0ba702c2c866f4ce0832ddb8b5e0
liulu <532922397@qq.com>
2017-01-23 17:30:46 +0800

    Merge remote-tracking branch 'Accumulations/master'
    Conflicts:
    	.idea/gradle.xml
    	.idea/misc.xml
    	.idea/modules.xml
    	app/build.gradle
    	app/src/main/AndroidManifest.xml
    	app/src/main/java/com/example/liulu/accumulations/MainActivity.java
    	app/src/main/res/layout/activity_main.xml
    	app/src/main/res/values/colors.xml
    	app/src/main/res/values/dimens.xml
    	app/src/main/res/values/strings.xml
    	app/src/main/res/values/styles.xml
    	settings.gradle

---
+++ testlib/src/main/java/com/example/testlib/TestLib.java
@@ -0,0 +1,17 @@
+package com.example.testlib;
+
+/**
+ * Created by jh on 2017/1/10.
+ */
+
+public class TestLib {
+    private String name = "liulu";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
