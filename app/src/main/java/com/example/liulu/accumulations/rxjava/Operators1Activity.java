6193c67d1d28c629b71d64d7299d4e8780583326
liulu <532922397@qq.com>
2017-02-13 19:46:00 +0800

    Merge remote-tracking branch 'Accumulations/master'
    Conflicts:
    	.idea/gradle.xml
    	.idea/misc.xml
    	.idea/modules.xml
    	app/build.gradle
    	app/src/main/AndroidManifest.xml
    	app/src/main/java/com/example/liulu/accumulations/MainActivity.java
    	app/src/main/res/layout/activity_main.xml
    	app/src/main/res/values/colors.xml
    	app/src/main/res/values/dimens.xml
    	app/src/main/res/values/strings.xml
    	app/src/main/res/values/styles.xml
    	settings.gradle

--- app/src/main/java/com/example/liulu/accumulations/rxjava/Operators1Activity.java
+++ app/src/main/java/com/example/liulu/accumulations/rxjava/Operators1Activity.java
@@ -1,15 +1,42 @@
 package com.example.liulu.accumulations.rxjava;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentTransaction;
 
 import com.example.liulu.accumulations.R;
+import com.example.liulu.accumulations.common.BaseActivity;
+import com.example.liulu.accumulations.wiget.Log;
+import com.example.liulu.accumulations.wiget.LogFragment;
+import com.example.liulu.accumulations.wiget.LogWrapper;
+import com.example.liulu.accumulations.wiget.MessageOnlyLogFilter;
 
-public class Operators1Activity extends AppCompatActivity {
+public class Operators1Activity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_operators1);
+    protected void initData() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        Operators1Fragment fragment = new Operators1Fragment();
+        transaction.replace(R.id.sample_content_fragment, fragment);
+        transaction.commit();
+    }
+
+    @Override
+    public int getLayout() {
+        return R.layout.activity_scheduler;
+    }
+
+    @Override
+    public void initializeLogging() {
+        LogWrapper logWrapper = new LogWrapper();
+        Log.setLogNode(logWrapper);
+
+        MessageOnlyLogFilter msgFilter = new MessageOnlyLogFilter();
+        logWrapper.setNext(msgFilter);
+
+        // On screen logging via a fragment with a TextView.
+        LogFragment logFragment = (LogFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.log_fragment);
+        msgFilter.setNext(logFragment.getLogView());
+
+        Log.i("liulu", "Ready");
     }
 }
